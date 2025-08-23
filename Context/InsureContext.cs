e367d58e8ca4279705fd05a2e207c455af12f893
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-08-23 22:07:01 +0300

    ASP.NET Core ile Yapay Zeka Entegrasyonları: Sigorta Projesi
    Bölüm 1: Giriş ve Entitylerin Yazılması
    Giriş,
    Slider ve About Sınıfları,
    Pricing, Service, Slider ve TrailerVideo Sınıfları,
    Article ve Category Sınıfları,
    Entitylerin Tamamlanması,
    Paketlerin Kurulması ve Context Sınıfı
    Migration İşlemi

---
+++ Context/InsureContext.cs
@@ -0,0 +1,25 @@
+﻿using InsureYouAI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsureYouAI.Context
+{
+    public class InsureContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\hasan;initial catalog=InsureDb; integrated security=true;Trust server certificate=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AboutItem> AboutItems { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<PricingPlan> PricingPlans { get; set; }
+        public DbSet<Service> Sevices { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<TrailerVideo> TrailerVideos { get; set; }
+    }
+}
---
+++ Entities/PricingPlan.cs
@@ -0,0 +1,12 @@
+﻿namespace InsureYouAI.Entities
+{
+    public class PricingPlan
+    {
+        public int PricingPlanId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsFEature { get; set; }
+        //public string BillingCycle { get; set; } // e.g., Monthly, Yearly
+    }
+}
